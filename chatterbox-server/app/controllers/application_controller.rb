6403ea084e131c7652c7756a8fa0a3007b86639d
antoninaorobets <pani.antonina.don@gmail.com>
2022-05-22 11:35:33 -0400

    commit tree 747d77585d8a8b37b29e1359474ba64baab659a8

--- chatterbox-server/app/controllers/application_controller.rb
+++ chatterbox-server/app/controllers/application_controller.rb
@@ -2,5 +2,26 @@ class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
   # Add routes
+  get '/messages' do
+    # binding.pry
+    Message.all.order(:created_at).to_json
+  end
+
+  post '/messages' do
+    Message.create(body: params[:body], username: params[:username]).to_json
+  end
+
+  patch '/messages/:id' do
+    updated_message = Message.find(params[:id])
+    updated_message.update(body: params[:body])
+    updated_message.to_json
+  end
+
+  delete '/messages/:id' do
+   
+    remove = Message.find(params[:id])
+    remove.destroy
+  end
+ 
   
 end
